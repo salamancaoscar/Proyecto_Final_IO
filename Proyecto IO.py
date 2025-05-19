fd45599be5ce3959cde7c336bae8730e52dbc4d9
oscar salamanca <salamancaoscar519@gmail.com>
2025-05-18 19:37:14 -0500

    El menú ya funciona, se compeltó  el criterio de savage, falta el criterio de Hurwickz

--- Proyecto IO.py
+++ Proyecto IO.py
@@ -1,105 +1,205 @@
 
-import random
-
-print("Ingrese 0 para crear la matriz de forma manual\nIngrese 1 para crear la matriz de forma aleatoria");
-elegir = int(input());
-filas= int(input("\ningrese el numero de filas:  "))
-columnas = int(input("\ningrese el numero de  columnas:  "))
-matriz=[]
-fila_actual=[]
-combinacion = random.choice([random.randint, random.uniform])
-probabilidad = (1/columnas)
+mport random
 
 
-
-acumulador_fila=0;
-acumulador_columna=0;
-
-
-# para generar matriz manual
-if elegir == 0:
+def menu():
     
-    for i in  range(filas):
-        fila_actual=[];
-        for j in range (columnas):
-            valor = float(input((f"\nIngrese el valor para la fila{i}, columna{j}: ")));
-            fila_actual.append(valor)
-        matriz.append(fila_actual)
-    for fila in matriz:
-            print(f"\n{fila}")
+    print("\nIngrese una de las siguientes opciones: "
+          "\n1. Criterio de Laplace."
+          "\n2. Criterio Optimista."
+          "\n3. Criterio Pesimista."
+          "\n4. Criterio de Hurwicz."
+          "\n5. Criterio de Savage."
+          "\n6. Salir")    
+
+
+def seleccion():
+
+        print("Ingrese 0 para crear la matriz de forma manual\nIngrese 1 para crear la matriz de forma aleatoria\n");
+        elegir = int(input());
+        filas= int(input("\ningrese el numero de filas:  \n"))
+        columnas = int(input("\ningrese el numero de  columnas:  \n"))
+        matriz=[]
+        fila_actual=[]
+        combinacion = random.choice([random.randint, random.uniform])
+        probabilidad = (1/columnas)
     
 
+    # para generar matriz manual
+        if elegir == 0:
+    
+            for i in  range(filas):
+                fila_actual=[];
+                for j in range (columnas):
+                    valor = float(input((f"\nIngrese el valor para la fila{i}, columna{j}: ")));
+                    fila_actual.append(valor)
+                matriz.append(fila_actual)
+            for fila in matriz:
+                print(f"\n{fila}\n")
+
+            return matriz,filas,columnas,probabilidad
+        
+        
 
 # para generar matriz aleatoria
 
-elif elegir == 1:
+        elif elegir == 1:
 
-    for i in  range(filas):
-        fila_actual=[];
-        for j in range (columnas):
-            valor = combinacion(1,100)
-            fila_actual.append(valor)
-        matriz.append(fila_actual)
-    for fila in matriz:
-        print(f"\n{fila}" )
+            for i in  range(filas):
+                fila_actual=[];
+                for j in range (columnas):
+                    valor = combinacion(-5000,5000)
+                    fila_actual.append(valor)
+                matriz.append(fila_actual)
+            for fila in matriz:
+                print(f"\n{fila}\n")
+        
+                
+            return matriz,filas,columnas,probabilidad
+              
+while True:
+    menu()
     
+    try:
 
-#criterio de  Laplace
+        opcion = int(input())
+        
 
-#valor_Laplace=0;
+#criterio de Laplace
 
-#for k in range(filas):
-    #valor_Mayor = 0
-    #for l in range(columnas):
-        #valor_Mayor += matriz[k][l] * probabilidad
+        if opcion == 1:
+            print("Usted ha elegido el criterio de Laplace\n")
+
+            matriz,filas, columnas, probabilidad =seleccion()
+
+            valor_Laplace=0;
+
+            for k in range(filas):
+                valor_mayor = 0
+                for l in range(columnas):
+                    valor_mayor += matriz[k][l] * probabilidad
       
-    #print(f"\nAlternativa {k+1} = {valor_Mayor}")
+                print(f"\nAlternativa {k+1} = {valor_mayor}")
 
-    #if suma >valor_Laplace:
-        #valor_Laplace = suma
-#print(f"\nEl valor esperado es:  {valor_Laplace}")
-
-
-
-#criterio de  optimista
-
-
-#valor_optimista = 0
-
-
-#for n in range (columnas):
-    #comparar_columnas = 0
-    #for m in range(filas):
-        #comparar_columnas= matriz[m][n]
-        #if matriz[m][n]> comparar_columnas:
-            #comparar_columnas = matriz[m][n]
-    #print(f"\nAlternativa {n+1} = {comparar_columnas}")
+                if valor_mayor >valor_Laplace:
+                    valor_Laplace = valor_mayor
+            print(f"\nEl valor esperado es:  {valor_Laplace}")
             
-    #if comparar_columnas>valor_optimista:
-        #valor_optimista= comparar_columnas
+        
+#Citerio Optimista
+                 
+        elif opcion == 2:
+            print("\nUsted ha elegido el criterio de Optimista\n")
 
-       
-#print(f"\nEl mejor valor es : {valor_optimista}")
-       
+            matriz,columnas,filas,_ = seleccion()
 
-#criterio pesimista
-
-#valor_pesimista = -float("inf")
-
-#for o in range (filas):
-    #valor_minimo = matriz[o][0]
-    
-    #for p in range(1,columnas):
-        #if matriz[o][p]<valor_minimo:
-            #valor_minimo= matriz[o][p]
+            valor_optimista = 0
+            for m in range (filas):
+                comparar_fila = matriz[m][0]
+                for n in range(1,filas):
+                    if matriz[m][n]> comparar_fila:
+                        comparar_fila = matriz[m][n]
+                print(f"\nAlternativa {n+1} = {comparar_fila}")
             
-    #print(f"\nAlternativa {o+1} = {valor_minimo}")
+                if comparar_fila>valor_optimista:
+                    valor_optimista= comparar_fila
+
+       
+            print(f"\nEl mejor valor es : {valor_optimista}")
             
-#if valor_minimo>valor_pesimista:
-    #valor_pesimista = valor_minimo
+       
+#Citerio Pesimista
+            
+                 
+        elif opcion == 3:
+            print("\nUsted ha elegido el criterio pesimista\n")
+
+            matriz,columnas,filas,_ = seleccion()
+
+            valor_pesimista = -float("inf")
+        
+            for o in range (filas):
+                valor_minimo = matriz[o][0]
+                for p in range(1,columnas):
+                    if matriz[o][p]<valor_minimo:
+                        valor_minimo= matriz[o][p]
+            
+                print(f"\nAlternativa {o+1} = {valor_minimo}") 
+            
+                if valor_minimo>valor_pesimista:
+                    valor_pesimista = valor_minimo
 
        
-#print(f"\nSégun el criterio pesimista se debe elegir la alternativa: {valor_minimo}")
-       
+            print(f"\nSégun el criterio pesimista se debe elegir la alternativa: {valor_pesimista}")
+                
+        #elif opcion == 4:
+            #print("\nUsted ha elegido el criterio de Hurwicz\n")
 
+
+
+
+
+
+
+
+
+
+
+ #criterio de savage
+            
+
+        elif opcion == 5:
+            print("\nUsted ha elegido el criterio de Savage\n")
+
+            matriz,filas,columnas,matriz_savage = seleccion()
+
+
+            resta_filas=0
+            valores_filas=[]
+            valor_max_columna=[]
+            matriz_savage=[]
+
+            valor_savage=0
+
+            for t in range(columnas):
+                valor_maximo = matriz[0][t]
+                for s in range(filas):
+                    if matriz[s][t] > valor_maximo:
+                        valor_maximo = matriz[s][t]
+                valor_max_columna.append(valor_maximo)
+                        
+            for s in range(filas):
+                nueva_matriz=[]
+                for t in range (columnas):
+                    resta_filas= valor_max_columna[t] - matriz[s][t]
+                    nueva_matriz.append(resta_filas)
+                matriz_savage.append(nueva_matriz)
+
+            print(f"\nMatriz de Beneficios Esperados\n")
+            for fila in matriz_savage:
+                print(f"\n{fila}\n")
+
+
+            valores_filas.sort()
+            for i in range(len(matriz_savage)):
+                numero_mayor= matriz_savage[i]
+                valor_maximo_fila = max(numero_mayor)
+                valores_filas.append(valor_maximo_fila)
+                print(f"\nAlternativa {i+1} = {valor_maximo_fila} ")
+
+            valores_filas.sort()
+            valor_central = (valores_filas[1])
+            print(f"\nEl valor Esperado es:{valor_central}")
+
+
+        #elif opcion == 6:
+            #print("\n¡Gracias, vuelva pronto\n")
+            #break
+        #else:
+           # print("\n¡Opción inválida! Por favor, ingrese una opción válida (1 a 6).\n")
+           # menu()
+
+    except ValueError:
+            print("\n¡Entrada inválida! Por favor, ingrese una opción válida (1 a 6)\n")
+           
 
